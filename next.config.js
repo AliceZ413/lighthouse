d8471af29e5a0b173e70ed0cab7b195afff95cf8
AndyZ <zhangy01@softin-tech.com>
2023-09-14 14:49:50 +0800

    chore: init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const nextra = require('nextra');
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withNextra(nextConfig);
