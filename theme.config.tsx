e36a420c76b58c753ebb48c8827cdfc459eb775f
AndyZ <zhangy01@softin-tech.com>
2023-09-14 15:02:53 +0800

    chore: change [theme.config.jsx] to [theme.config.tsx]

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import type { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Logo</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nobody knows who I really am. - Life Is Like A Boat',
+  },
+};
+
+export default config;
